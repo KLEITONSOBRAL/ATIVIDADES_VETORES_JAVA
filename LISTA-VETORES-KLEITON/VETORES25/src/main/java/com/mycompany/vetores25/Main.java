aa9c6c4193c417f2e96fd8539c4a86c3ff26b925
Kleiton Sobral <kleiton.c@aluno.senai.br>
2026-07-13 10:25:43 -0300

    Add month name retrieval functionality
    Added a comment explaining the program's functionality and corrected the formatting of the month array.

--- LISTA-VETORES-KLEITON/VETORES25/src/main/java/com/mycompany/vetores25/Main.java
+++ LISTA-VETORES-KLEITON/VETORES25/src/main/java/com/mycompany/vetores25/Main.java
@@ -1,14 +1,22 @@
+/*
+Faça um programa em Java que leia um número inteiro correspondente a um mês do ano, considerando valores de **1 a 12**.
+Em seguida, o programa deverá exibir o nome do mês correspondente ao número informado.
+Caso o valor digitado esteja fora do intervalo de 1 a 12, deverá ser exibida uma mensagem informando que o número do mês é inválido.
+*/
+
 package com.mycompany.vetores25;
 
 import java.util.Scanner;
 
 public class Main {
+    
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
 
         String[] meses = {
-            "", "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", 
-            "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+                "", "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+                "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
         };
 
         System.out.print("Digite o número do mês (1 a 12): ");
