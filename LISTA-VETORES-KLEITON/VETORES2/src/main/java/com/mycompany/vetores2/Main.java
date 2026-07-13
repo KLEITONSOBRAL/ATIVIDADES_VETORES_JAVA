4e4be4fb72a4de35f52b29581f199e12f83c071f
Kleiton Sobral <kleiton.c@aluno.senai.br>
2026-07-13 06:37:08 -0300

    Implement array initialization and display in descending order
    Added a comment describing the program's functionality.

--- LISTA-VETORES-KLEITON/VETORES2/src/main/java/com/mycompany/vetores2/Main.java
+++ LISTA-VETORES-KLEITON/VETORES2/src/main/java/com/mycompany/vetores2/Main.java
@@ -1,18 +1,24 @@
+/*
+Crie um programa em Java que declare um vetor de números inteiros com 100 posições.
+Preencha o vetor com os números de 100 até 1, em ordem decrescente, e depois exiba todos os valores armazenados.
+*/
+
 package com.mycompany.vetores2;
 
 public class Main {
+
     public static void main(String[] args) {
 
         int[] numeros = new int[100];
-        
 
         for (int i = 0; i < 100; i++) {
-            numeros[i] = 100 - i; 
+            numeros[i] = 100 - i;
         }
-        
-        
+
         System.out.println("Números armazenados no vetor (de 100 a 1):");
+
         for (int i = 0; i < 100; i++) {
+            
             System.out.println(numeros[i]);
         }
     }
