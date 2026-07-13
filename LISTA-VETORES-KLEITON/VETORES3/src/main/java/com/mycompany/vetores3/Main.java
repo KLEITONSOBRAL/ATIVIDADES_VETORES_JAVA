67e44a21031aa072526642db595832df58e2256a
Kleiton Sobral <kleiton.c@aluno.senai.br>
2026-07-13 06:39:10 -0300

    Store even numbers from 1 to 100 in an array
    Refactor the main method to properly store even numbers in an array.

--- LISTA-VETORES-KLEITON/VETORES3/src/main/java/com/mycompany/vetores3/Main.java
+++ LISTA-VETORES-KLEITON/VETORES3/src/main/java/com/mycompany/vetores3/Main.java
@@ -1,20 +1,27 @@
+/*
+Crie um programa em Java que declare um vetor com 50 posições,
+armazene nele todos os números pares existentes no intervalo de 1 a 100 e, ao final,
+exiba os valores armazenados no vetor.
+*/
+
 package com.mycompany.vetores3;
 
 public class Main {
+
     public static void main(String[] args) {
-        
-        int[] pares = new int[50]; 
-        
-        
-        int index = 0;  
-        for (int i = 2; i <= 100; i += 2) {  
+
+        int[] pares = new int[50];
+
+        int index = 0;
+        for (int i = 2; i <= 100; i += 2) {
             pares[index] = i;
             index++;
         }
-        
-        
+
         System.out.println("Números pares no intervalo de 1 a 100:");
+
         for (int i = 0; i < pares.length; i++) {
+            
             System.out.println(pares[i]);
         }
     }
