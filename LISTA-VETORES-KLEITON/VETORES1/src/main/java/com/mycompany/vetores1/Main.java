ab3ffb7c0754b979f69b373d1fb758e3ed5432bd
Kleiton Sobral <kleiton.c@aluno.senai.br>
2026-07-13 06:28:10 -0300

    Implement array initialization and display in Java
    Added a Java program to create and display an array of integers from 0 to 50.

--- LISTA-VETORES-KLEITON/VETORES1/src/main/java/com/mycompany/vetores1/Main.java
+++ LISTA-VETORES-KLEITON/VETORES1/src/main/java/com/mycompany/vetores1/Main.java
@@ -1,17 +1,22 @@
+/*
+Crie um programa em Java que declare um vetor de números inteiros com 51 posições.
+Preencha o vetor com os números de 0 a 50 e, em seguida, exiba todos os valores armazenados.
+*/
+
 package com.mycompany.vetores1;
 
 public class Main {
+
     public static void main(String[] args) {
-        
+
         int[] numeros = new int[51];
-        
-        
+
         for (int i = 0; i <= 50; i++) {
             numeros[i] = i;
         }
-        
-        
+
         System.out.println("Números armazenados no vetor:");
+        
         for (int i = 0; i <= 50; i++) {
             System.out.println(numeros[i]);
         }
