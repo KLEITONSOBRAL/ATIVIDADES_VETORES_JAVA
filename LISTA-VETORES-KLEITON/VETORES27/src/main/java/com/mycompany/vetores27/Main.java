497938df125cfa53793aab618321035f91e6ffc3
Kleiton Sobral <kleiton.c@aluno.senai.br>
2026-07-13 10:28:45 -0300

    Implement array input and copying for 1000 integers
    Added functionality to read 1000 integers into an array and copy them to a second array.

--- LISTA-VETORES-KLEITON/VETORES27/src/main/java/com/mycompany/vetores27/Main.java
+++ LISTA-VETORES-KLEITON/VETORES27/src/main/java/com/mycompany/vetores27/Main.java
@@ -1,26 +1,29 @@
+/*
+Faça um programa em Java que leia e armazene **1.000 números inteiros** em um vetor. 
+Em seguida, copie todos os elementos desse vetor para um segundo vetor, mantendo os valores nas mesmas posições.
+*/
+
 package com.mycompany.vetores27;
 
 import java.util.Scanner;
 
 public class Main {
+    
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
 
         int[] originalArray = new int[1000];
         int[] copiedArray = new int[1000];
-
         
         System.out.println("Digite 1000 números inteiros:");
         for (int i = 0; i < 1000; i++) {
             originalArray[i] = scanner.nextInt();
         }
-
         
         for (int i = 0; i < 1000; i++) {
             copiedArray[i] = originalArray[i];
         }
-
-  
 
         scanner.close();
     }
