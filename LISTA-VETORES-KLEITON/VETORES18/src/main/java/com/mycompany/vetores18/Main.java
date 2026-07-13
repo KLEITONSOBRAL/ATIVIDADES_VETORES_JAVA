ddef602deb50990954f0e0c9b238dc715a4ef2f6
Kleiton Sobral <kleiton.c@aluno.senai.br>
2026-07-13 09:58:23 -0300

    Implement duplicate number detection in array
    Added functionality to read 50 integers and identify duplicates with their positions.

--- LISTA-VETORES-KLEITON/VETORES18/src/main/java/com/mycompany/vetores18/Main.java
+++ LISTA-VETORES-KLEITON/VETORES18/src/main/java/com/mycompany/vetores18/Main.java
@@ -1,21 +1,28 @@
+/*
+Faça um programa em Java que leia e armazene **50 números inteiros** em um vetor.
+Em seguida, verifique quais números aparecem mais de uma vez e mostre o valor repetido
+e as posições em que ele foi encontrado.
+Caso não existam números repetidos, exiba uma mensagem informando essa situação.
+*/
+
 package com.mycompany.vetores18;
 
 import java.util.Scanner;
 
 public class Main {
+    
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
 
         int[] VET = new int[50];
         boolean repetido = false;
-
         
         System.out.println("Digite 50 números para o vetor:");
         for (int i = 0; i < 50; i++) {
             System.out.print("Número " + (i + 1) + ": ");
             VET[i] = scanner.nextInt();
         }
-
         
         System.out.println("\nNúmeros repetidos e suas posições:");
 
@@ -24,7 +31,7 @@ public class Main {
                 if (VET[i] == VET[j]) {
                     System.out.println("Número " + VET[i] + " repetido nas posições " + (i + 1) + " e " + (j + 1));
                     repetido = true;
-                    break; 
+                    break;
                 }
             }
         }
