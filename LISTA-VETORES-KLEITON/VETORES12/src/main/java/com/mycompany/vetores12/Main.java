1aa0101675d193445ef880f612cf8fc92d88a61a
Kleiton Sobral <kleiton.c@aluno.senai.br>
2026-07-13 09:41:55 -0300

    Add program to read and display numbers in reverse
    Implemented a Java program to read and display 20 integers in reverse order.

--- LISTA-VETORES-KLEITON/VETORES12/src/main/java/com/mycompany/vetores12/Main.java
+++ LISTA-VETORES-KLEITON/VETORES12/src/main/java/com/mycompany/vetores12/Main.java
@@ -1,20 +1,29 @@
+/*
+Faça um programa em Java que leia e armazene **20 números inteiros** em um vetor.
+Em seguida, mostre todos os elementos do vetor na **ordem inversa àquela em que foram digitados**.
+*/
+
 package com.mycompany.vetores12;
 
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
         int[] numeros = new int[20];
 
         System.out.println("Digite 20 números:");
+
         for (int i = 0; i < 20; i++) {
             System.out.print("Número " + (i + 1) + ": ");
             numeros[i] = scanner.nextInt();
         }
 
         System.out.println("\nNúmeros na ordem inversa:");
+        
         for (int i = 19; i >= 0; i--) {
             System.out.println("Número " + (i + 1) + ": " + numeros[i]);
         }
