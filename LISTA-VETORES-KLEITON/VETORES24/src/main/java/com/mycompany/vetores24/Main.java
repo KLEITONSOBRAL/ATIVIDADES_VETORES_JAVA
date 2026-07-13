a800207049e3a7d7264996543e8933d1b7ded089
Kleiton Sobral <kleiton.c@aluno.senai.br>
2026-07-13 10:24:31 -0300

    Add program description for energy cost calculation
    Added a comment block explaining the program's functionality.

--- LISTA-VETORES-KLEITON/VETORES24/src/main/java/com/mycompany/vetores24/Main.java
+++ LISTA-VETORES-KLEITON/VETORES24/src/main/java/com/mycompany/vetores24/Main.java
@@ -1,9 +1,20 @@
+/*
+Faça um programa em Java que leia o valor cobrado por **quilowatt (kW)** e a quantidade de consumidores que serão cadastrados.
+Para cada consumidor, o programa deverá solicitar:
+* O nome do consumidor;
+* A quantidade de quilowatts consumidos.
+Em seguida, deverá calcular e exibir o valor total da conta de energia de cada consumidor, utilizando a fórmula:
+**Total a pagar = valor do kW × quantidade de kW consumidos**
+*/
+
 package com.mycompany.vetores24;
 
 import java.util.Scanner;
 
 public class Main {
+    
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Digite o valor do Kw: ");
