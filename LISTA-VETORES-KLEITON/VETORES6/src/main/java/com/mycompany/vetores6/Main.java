880007aa74e3e9cb844b2799a9749cc11a51b96f
Kleiton Sobral <kleiton.c@aluno.senai.br>
2026-07-13 06:51:42 -0300

    Refactor array manipulation and output logic
    Refactor the loop to swap elements and update values in the array. Adjust the final output to display the contents of the array correctly.

--- LISTA-VETORES-KLEITON/VETORES6/src/main/java/com/mycompany/vetores6/Main.java
+++ LISTA-VETORES-KLEITON/VETORES6/src/main/java/com/mycompany/vetores6/Main.java
@@ -1,26 +1,31 @@
+/*
+Considere o vetor `V = {5, 1, 4, 2, 7, 8, 3, 6}`. 
+Desenvolva um programa em Java que realize operações de troca e atribuição entre 
+os elementos do vetor e, ao final, exiba o conteúdo de todas as suas posições.
+*/
+
 package com.mycompany.vetores6;
 
 public class Main {
+
     public static void main(String[] args) {
-        
+
         int[] v = {5, 1, 4, 2, 7, 8, 3, 6};
-        
-        
-        for (int i = 8; i >= 5; i--) {
+
+        for (int i = 7; i >= 4; i--) {
             int aux = v[i];
-            v[i] = v[8 - i + 1];
-            v[8 - i + 1] = aux;
+            v[i] = v[7 - i];
+            v[7 - i] = aux;
         }
-        
-        
-        v[3] = v[1];
-        
-        
-        v[v[3]] = v[v[2]];
-        
-        
+
+        v[2] = v[0];
+
+        v[v[2] - 1] = v[v[1] - 1];
+
         System.out.println("Conteúdo final do vetor:");
+
         for (int i = 0; i < v.length; i++) {
+            
             System.out.println("v[" + i + "] = " + v[i]);
         }
     }
