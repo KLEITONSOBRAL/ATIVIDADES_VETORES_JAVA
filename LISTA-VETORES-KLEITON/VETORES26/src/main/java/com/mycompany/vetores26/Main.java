73116d3d0e85323c6d397d5f2e4a7ffcf09f2f17
Kleiton Sobral <kleiton.c@aluno.senai.br>
2026-07-13 10:27:20 -0300

    Add number input and search functionality
    Implement logic to read 100 integers and search for a number in the array, displaying its first occurrence.

--- LISTA-VETORES-KLEITON/VETORES26/src/main/java/com/mycompany/vetores26/Main.java
+++ LISTA-VETORES-KLEITON/VETORES26/src/main/java/com/mycompany/vetores26/Main.java
@@ -1,24 +1,30 @@
+/*
+Faça um programa em Java que leia e armazene **100 números inteiros** em um vetor.
+Em seguida, solicite ao usuário um número a ser pesquisado no vetor.
+O programa deverá informar a posição da **primeira ocorrência** do número procurado. 
+Caso ele não seja encontrado, deverá exibir uma mensagem informando que o número não existe no vetor.
+*/
+
 package com.mycompany.vetores26;
 
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
         int[] numeros = new int[100];
 
-        
         System.out.println("Digite 100 números inteiros:");
         for (int i = 0; i < 100; i++) {
             numeros[i] = scanner.nextInt();
         }
 
-        
         System.out.print("Digite o número a ser buscado: ");
         int numeroProcurado = scanner.nextInt();
 
-        
         boolean encontrado = false;
         for (int i = 0; i < 100; i++) {
             if (numeros[i] == numeroProcurado) {
